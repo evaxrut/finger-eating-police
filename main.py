3d6b809c2fd91f33c61be88a8f9f68737c8c0593
evaxrut <136167068+evaxrut@users.noreply.github.com>
2025-03-04 20:55:58 +0100

    initial

--- main.py
+++ main.py
@@ -1,5 +1,164 @@
+from cv2.gapi.wip import draw
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe import solutions
+from mediapipe.tasks.python import vision
+from pathlib import Path
+from mediapipe.framework.formats import landmark_pb2
+import numpy as np
+import cv2
+
+BaseOptions = mp.tasks.BaseOptions
+HandLandmarker = mp.tasks.vision.HandLandmarker
+HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
+VisionRunningMode = mp.tasks.vision.RunningMode
+FaceLandmarker = mp.tasks.vision.FaceLandmarker
+FaceLandmarkerOptions = mp.tasks.vision.FaceLandmarkerOptions
+
+MARGIN = 10  # pixels
+FONT_SIZE = 1
+FONT_THICKNESS = 1
+HANDEDNESS_TEXT_COLOR = (88, 205, 54)  # vibrant green
+
+HAND_MODEL_PATH = Path("./models/hand_landmarker.task")
+FACE_MODEL_PATH = Path("./models/face_landmarker.task")
+
+
+def draw_hand_landmarks_on_image(rgb_image, detection_result):
+    hand_landmarks_list = detection_result.hand_landmarks
+    handedness_list = detection_result.handedness
+    annotated_image = np.copy(rgb_image)
+
+    # Loop through the detected hands to visualize.
+    for idx in range(len(hand_landmarks_list)):
+        hand_landmarks = hand_landmarks_list[idx]
+        handedness = handedness_list[idx]
+
+        # Draw the hand landmarks.
+        hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+        hand_landmarks_proto.landmark.extend(
+            [
+                landmark_pb2.NormalizedLandmark(
+                    x=landmark.x, y=landmark.y, z=landmark.z
+                )
+                for landmark in hand_landmarks
+            ]
+        )
+        solutions.drawing_utils.draw_landmarks(
+            annotated_image,
+            hand_landmarks_proto,
+            solutions.hands.HAND_CONNECTIONS,
+            solutions.drawing_styles.get_default_hand_landmarks_style(),
+            solutions.drawing_styles.get_default_hand_connections_style(),
+        )
+
+        # Get the top left corner of the detected hand's bounding box.
+        height, width, _ = annotated_image.shape
+        x_coordinates = [landmark.x for landmark in hand_landmarks]
+        y_coordinates = [landmark.y for landmark in hand_landmarks]
+        text_x = int(min(x_coordinates) * width)
+        text_y = int(min(y_coordinates) * height) - MARGIN
+
+        # Draw handedness (left or right hand) on the image.
+        cv2.putText(
+            annotated_image,
+            f"{handedness[0].category_name}",
+            (text_x, text_y),
+            cv2.FONT_HERSHEY_DUPLEX,
+            FONT_SIZE,
+            HANDEDNESS_TEXT_COLOR,
+            FONT_THICKNESS,
+            cv2.LINE_AA,
+        )
+
+    return annotated_image
+
+
+def draw_face_landmarks_on_image(rgb_image, detection_result):
+    face_landmarks_list = detection_result.face_landmarks
+    annotated_image = np.copy(rgb_image)
+
+    # Loop through the detected faces to visualize.
+    for idx in range(len(face_landmarks_list)):
+        face_landmarks = face_landmarks_list[idx]
+
+        # Draw the face landmarks.
+        face_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+        face_landmarks_proto.landmark.extend(
+            [
+                landmark_pb2.NormalizedLandmark(
+                    x=landmark.x, y=landmark.y, z=landmark.z
+                )
+                for landmark in face_landmarks
+            ]
+        )
+
+        solutions.drawing_utils.draw_landmarks(
+            image=annotated_image,
+            landmark_list=face_landmarks_proto,
+            connections=mp.solutions.face_mesh.FACEMESH_TESSELATION,
+            landmark_drawing_spec=None,
+            connection_drawing_spec=mp.solutions.drawing_styles.get_default_face_mesh_tesselation_style(),
+        )
+        solutions.drawing_utils.draw_landmarks(
+            image=annotated_image,
+            landmark_list=face_landmarks_proto,
+            connections=mp.solutions.face_mesh.FACEMESH_CONTOURS,
+            landmark_drawing_spec=None,
+            connection_drawing_spec=mp.solutions.drawing_styles.get_default_face_mesh_contours_style(),
+        )
+        solutions.drawing_utils.draw_landmarks(
+            image=annotated_image,
+            landmark_list=face_landmarks_proto,
+            connections=mp.solutions.face_mesh.FACEMESH_IRISES,
+            landmark_drawing_spec=None,
+            connection_drawing_spec=mp.solutions.drawing_styles.get_default_face_mesh_iris_connections_style(),
+        )
+
+    return annotated_image
+
+
 def main():
-    print("Hello from fingering-police!")
+    hand_options = HandLandmarkerOptions(
+        base_options=BaseOptions(model_asset_path=str(HAND_MODEL_PATH)),
+        running_mode=VisionRunningMode.IMAGE,
+        num_hands=2,
+        min_hand_detection_confidence=0.3,
+        min_tracking_confidence=0.3,
+        min_hand_presence_confidence=0.3,
+    )
+    face_options = vision.FaceLandmarkerOptions(
+        base_options=BaseOptions(model_asset_path=str(FACE_MODEL_PATH)),
+        output_face_blendshapes=True,
+        output_facial_transformation_matrixes=True,
+        num_faces=1,
+    )
+
+    hand_detector = HandLandmarker.create_from_options(hand_options)
+    face_detector = vision.FaceLandmarker.create_from_options(face_options)
+
+    webcam_cap = cv2.VideoCapture(0)
+
+    while True:
+        _, frame = webcam_cap.read()
+
+        frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_LINEAR)
+
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+        hand_detection = hand_detector.detect(mp_image)
+        face_detection = face_detector.detect(mp_image)
+
+        drawn = draw_hand_landmarks_on_image(frame, hand_detection)
+        drawn = draw_face_landmarks_on_image(drawn, face_detection)
+
+        cv2.imshow("cam", drawn)
+
+        key = cv2.waitKey(1)
+        if key & 0xFF == ord("q"):
+            break
+
+    webcam_cap.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
