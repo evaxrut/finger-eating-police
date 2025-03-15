4bd3e096bf12652f1eefadc4e51f8b525e8838ca
evaxrut <136167068+evaxrut@users.noreply.github.com>
2025-03-15 19:34:13 +0100

    seperate files]

---
+++ constants.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+
+MARGIN = 10  # pixels
+FONT_SIZE = 1
+FONT_THICKNESS = 1
+HANDEDNESS_TEXT_COLOR = (88, 205, 54)  # vibrant green
+
+HAND_MODEL_PATH = Path("./models/hand_landmarker.task")
+FACE_MODEL_PATH = Path("./models/face_landmarker.task")
--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-from cv2.gapi.wip import draw
 import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe import solutions
 from mediapipe.tasks.python import vision
-from pathlib import Path
-from mediapipe.framework.formats import landmark_pb2
-import numpy as np
 import cv2
+from hand_landmarks import draw_hand_landmarks_on_image
+from face_landmarks import draw_face_landmarks_on_image
+from constants import HAND_MODEL_PATH, FACE_MODEL_PATH
 
 BaseOptions = mp.tasks.BaseOptions
 HandLandmarker = mp.tasks.vision.HandLandmarker
@@ -14,108 +11,6 @@ HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
 VisionRunningMode = mp.tasks.vision.RunningMode
 FaceLandmarker = mp.tasks.vision.FaceLandmarker
 FaceLandmarkerOptions = mp.tasks.vision.FaceLandmarkerOptions
-
-MARGIN = 10  # pixels
-FONT_SIZE = 1
-FONT_THICKNESS = 1
-HANDEDNESS_TEXT_COLOR = (88, 205, 54)  # vibrant green
-
-HAND_MODEL_PATH = Path("./models/hand_landmarker.task")
-FACE_MODEL_PATH = Path("./models/face_landmarker.task")
-
-
-def draw_hand_landmarks_on_image(rgb_image, detection_result):
-    hand_landmarks_list = detection_result.hand_landmarks
-    handedness_list = detection_result.handedness
-    annotated_image = np.copy(rgb_image)
-
-    # Loop through the detected hands to visualize.
-    for idx in range(len(hand_landmarks_list)):
-        hand_landmarks = hand_landmarks_list[idx]
-        handedness = handedness_list[idx]
-
-        # Draw the hand landmarks.
-        hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-        hand_landmarks_proto.landmark.extend(
-            [
-                landmark_pb2.NormalizedLandmark(
-                    x=landmark.x, y=landmark.y, z=landmark.z
-                )
-                for landmark in hand_landmarks
-            ]
-        )
-        solutions.drawing_utils.draw_landmarks(
-            annotated_image,
-            hand_landmarks_proto,
-            solutions.hands.HAND_CONNECTIONS,
-            solutions.drawing_styles.get_default_hand_landmarks_style(),
-            solutions.drawing_styles.get_default_hand_connections_style(),
-        )
-
-        # Get the top left corner of the detected hand's bounding box.
-        height, width, _ = annotated_image.shape
-        x_coordinates = [landmark.x for landmark in hand_landmarks]
-        y_coordinates = [landmark.y for landmark in hand_landmarks]
-        text_x = int(min(x_coordinates) * width)
-        text_y = int(min(y_coordinates) * height) - MARGIN
-
-        # Draw handedness (left or right hand) on the image.
-        cv2.putText(
-            annotated_image,
-            f"{handedness[0].category_name}",
-            (text_x, text_y),
-            cv2.FONT_HERSHEY_DUPLEX,
-            FONT_SIZE,
-            HANDEDNESS_TEXT_COLOR,
-            FONT_THICKNESS,
-            cv2.LINE_AA,
-        )
-
-    return annotated_image
-
-
-def draw_face_landmarks_on_image(rgb_image, detection_result):
-    face_landmarks_list = detection_result.face_landmarks
-    annotated_image = np.copy(rgb_image)
-
-    # Loop through the detected faces to visualize.
-    for idx in range(len(face_landmarks_list)):
-        face_landmarks = face_landmarks_list[idx]
-
-        # Draw the face landmarks.
-        face_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-        face_landmarks_proto.landmark.extend(
-            [
-                landmark_pb2.NormalizedLandmark(
-                    x=landmark.x, y=landmark.y, z=landmark.z
-                )
-                for landmark in face_landmarks
-            ]
-        )
-
-        solutions.drawing_utils.draw_landmarks(
-            image=annotated_image,
-            landmark_list=face_landmarks_proto,
-            connections=mp.solutions.face_mesh.FACEMESH_TESSELATION,
-            landmark_drawing_spec=None,
-            connection_drawing_spec=mp.solutions.drawing_styles.get_default_face_mesh_tesselation_style(),
-        )
-        solutions.drawing_utils.draw_landmarks(
-            image=annotated_image,
-            landmark_list=face_landmarks_proto,
-            connections=mp.solutions.face_mesh.FACEMESH_CONTOURS,
-            landmark_drawing_spec=None,
-            connection_drawing_spec=mp.solutions.drawing_styles.get_default_face_mesh_contours_style(),
-        )
-        solutions.drawing_utils.draw_landmarks(
-            image=annotated_image,
-            landmark_list=face_landmarks_proto,
-            connections=mp.solutions.face_mesh.FACEMESH_IRISES,
-            landmark_drawing_spec=None,
-            connection_drawing_spec=mp.solutions.drawing_styles.get_default_face_mesh_iris_connections_style(),
-        )
-
-    return annotated_image
 
 
 def main():
